82875933d956c34bb11d8e43a4cc625fbc4acc34
tanthah <165071150+tanthah@users.noreply.github.com>
2025-12-05 01:16:15 +0700

    Add files via upload

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,3 +1,4 @@
+// frontend/src/App.jsx - UPDATED VERSION
 import React from 'react'
 import { Routes, Route, Navigate } from 'react-router-dom'
 import { useSelector } from 'react-redux'
@@ -9,25 +10,37 @@ import ForgotPassword from './pages/ForgotPassword.jsx'
 import ReviewProfile from './pages/ReviewProfile.jsx'
 import EditProfile from './pages/EditProfile.jsx'
 import ProductDetail from './pages/ProductDetail.jsx'
+import About from './pages/About.jsx'
 
 function App() {
   const token = useSelector((s) => s.auth.token)
 
   return (
     <Routes>
+      {/* Public Routes */}
       <Route path="/" element={<Home />} />
+      <Route path="/home" element={<Home />} />
+      <Route path="/dashboard" element={<Home />} />
+      <Route path="/about" element={<About />} />
       <Route path="/login" element={<Login />} />
       <Route path="/register" element={<Register />} />
       <Route path="/forgot-password" element={<ForgotPassword />} />
-      <Route path="/review-profile" element={token ? <ReviewProfile /> : <Navigate to="/login" replace />} />
-      <Route path="/edit-profile" element={token ? <EditProfile /> : <Navigate to="/login" replace />} />
       <Route path="/product/:id" element={<ProductDetail />} />
 
-      <Route path="/home" element={<Home />} />
+      {/* Protected Routes */}
+      <Route 
+        path="/review-profile" 
+        element={token ? <ReviewProfile /> : <Navigate to="/login" replace />} 
+      />
+      <Route 
+        path="/edit-profile" 
+        element={token ? <EditProfile /> : <Navigate to="/login" replace />} 
+      />
 
+      {/* Fallback Route */}
+      <Route path="*" element={<Navigate to="/" replace />} />
     </Routes>
   )
 }
 
 export default App
-
--- frontend/src/pages/ProductDetail.jsx
+++ frontend/src/pages/ProductDetail.jsx
@@ -2,14 +2,14 @@ import React, { useEffect, useState } from 'react'
 import { Container, Row, Col, Button, Badge, Spinner, Alert } from 'react-bootstrap'
 import { useParams, useNavigate } from 'react-router-dom'
 import { useDispatch, useSelector } from 'react-redux'
-import { fetchProductById, incrementProductView } from '../redux/productSlice'
+import { fetchProductById } from '../redux/productSlice'
 import { Swiper, SwiperSlide } from 'swiper/react'
 import { Navigation, Pagination, Thumbs } from 'swiper/modules'
 import 'swiper/css'
 import 'swiper/css/navigation'
 import 'swiper/css/pagination'
 import 'swiper/css/thumbs'
-import './ProductDetail.css'
+import './css/ProductDetail.css'
 
 export default function ProductDetail() {
     const { id } = useParams()
@@ -23,8 +23,6 @@ export default function ProductDetail() {
     useEffect(() => {
         if (id) {
             dispatch(fetchProductById(id))
-            // Increment view count (silent)
-            dispatch(incrementProductView(id))
         }
     }, [id, dispatch])
 
@@ -106,7 +104,7 @@ export default function ProductDetail() {
             <Container className="py-4">
                 <Row className="g-4">
                     {/* Left Column - Images */}
-                    <Col lg={6}>
+                    <Col lg={5}>
                         <div className="product-images-section">
                             {/* Main Swiper */}
                             <Swiper
@@ -148,7 +146,7 @@ export default function ProductDetail() {
                     </Col>
 
                     {/* Right Column - Info */}
-                    <Col lg={6}>
+                    <Col lg={7}>
                         <div className="product-info-section">
                             {/* Product Name */}
                             <h1 className="product-title">{product.name}</h1>
